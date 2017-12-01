c487d7657d00a1a04fb15b92e7702c27aea71eba
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2017-12-01 13:06:19 +0100

    added solution for day 1 puzzle 2

---
+++ day_1_puzzle_2.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python
+# encoding: utf-8
+import numpy as np
+
+
+def solve_captcha(captcha):
+    if isinstance(captcha, str):
+        captcha = list(captcha)
+    if len(captcha) == 0:
+        return 0
+    cap = np.asarray(captcha, dtype=int)
+    mask = cap == np.roll(cap, np.floor(cap.size/2))
+    selection = cap[mask]
+    return np.sum(selection)
+
+
+def test_examples():
+    example_inputs = ["1212", "1221", "123425", "123123", "12131415"]
+    example_outputs = [6, 0, 4, 12, 4]
+
+    solution = [solve_captcha(example) for example in example_inputs]
+    assert solution == example_outputs
+
+def main():
+    test_examples()
+    test_input = ""
+    print(f"Solution for test input: {solve_captcha(test_input)}")
+
+if __name__ == '__main__':
+    main()
