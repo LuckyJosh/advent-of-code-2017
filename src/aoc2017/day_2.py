1fe046f884319ade47a9e9911381994acd386fb8
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2017-12-02 10:57:13 +0100

    added solution file for day 2

---
+++ src/aoc2017/day_2.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+# encoding: utf-8
+
+import click
+import numpy as np
+
+
+def checksum_puzzle_1(spreadsheet):
+
+
+
+@click.command()
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
