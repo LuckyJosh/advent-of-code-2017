d41087f56e8518d51ccc15d1e09e06fb25b895f5
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2017-12-03 21:12:55 +0100

    fixed the totally fluffed numbers for the special case array lengths

--- src/aoc2017/day_3.py
+++ src/aoc2017/day_3.py
@@ -50,7 +50,7 @@ def spiral_memory_steps_2(data):
     used_indicies_with_values = {}
 
     # @hack: There has to be a better way to do this!
-    additional_shape = [3, 4, 4, 5, 6]
+    additional_shape = [2, 2, 1, 1, 1]
     if data <= 5:
         shape = data + additional_shape[data-1]
     else:
