d83b68a02b13624369ba686394417a54b83f0432
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2017-12-03 01:22:30 +0100

    changed formatting of the given example input to match the real test input

--- tests/test_day_2_puzzle_1.py
+++ tests/test_day_2_puzzle_1.py
@@ -4,8 +4,6 @@
 from aoc2017.day_2 import checksum_puzzle_1
 
 def test_checksum_puzzle_1():
-    input_ = """5 1 9 5
-                7 5 3
-                2 4 6 8"""
+    input_ = "5\t1\t9\t5\n7\t5\t3\n2\t4\t6\t8"
     output = 18
     assert checksum_puzzle_1(input_) == output
