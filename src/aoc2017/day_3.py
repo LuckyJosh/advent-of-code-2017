249c2ad2d0d7581ae26b64678bd97e23b02f1b0c
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2017-12-03 16:36:25 +0100

    added solution file for day 3

---
+++ src/aoc2017/day_3.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+# encoding: utf-8
+
+import numpy as np
+
+from .download_input import get_input
+
+
+def spiral_memory_steps(data_location):
+    pass
+
+
+def main():
+    input_ = get_input(3)
+    print("Input:\n", input_)
+    print("Output:" )
+
+
+if __name__ == '__main__':
+    main()
