97e3a00645cf3971ff3bbb92ca7a804a97e785eb
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2017-12-03 20:40:39 +0100

    implemented solution for second puzzle

--- src/aoc2017/day_3.py
+++ src/aoc2017/day_3.py
@@ -40,11 +40,54 @@ def spiral_memory_steps_1(data_location):
     return num_steps
 
 
+def spiral_memory_steps_2(data):
+    data = int(data)
+
+    # internally to this function, data location start at 0 not at 1!
+
+    used_indicies_with_values = {}
+
+    indicies = np.zeros(shape=(data, 2), dtype=int)
+
+    first_indices = [(0, 0), (1, 0), (1, 1)]
+    first_values = [1, 1, 2]
+
+    moves = np.array([[0, 1], [1, 0], [0, -1], [-1, 0]])
+
+    neighbors = np.concatenate([moves, np.array([[1, 1], [1, -1], [-1, -1], [-1, 1]])])
+
+    for loc in range(0, data):
+        if loc <= 2:
+            used_indicies_with_values[first_indices[loc]] = first_values[loc]
+            indicies[loc, :] = first_indices[loc]
+        else:
+            current_index = indicies[loc-1, :]
+            new_indicies = current_index + moves
+            not_used = [tuple(idx) not in used_indicies_with_values.keys() for idx in new_indicies]
+            distance_to_center = np.linalg.norm(indicies[0, :] - new_indicies[not_used], axis=1)
+            min_distance = np.argmin(distance_to_center)
+
+            new_index = new_indicies[not_used][min_distance]
+            new_index_neighbors = new_index + neighbors
+            values_of_neighbors = [used_indicies_with_values[idx]
+                                   for idx in new_index_neighbors
+                                   if idx in used_indicies_with_values]
+
+            used_indicies_with_values[tuple(new_index)] = sum(values_of_neighbors)
+            indicies[loc, :] = new_indicies[not_used][min_distance]
+
+        if used_indicies_with_values[tuple(new_index)] > data:
+            return used_indicies_with_values[tuple(new_index)]
+
+
+
+
 @click.command()
 def main():
     input_ = get_input(3)
     print("Input:\n", input_)
     print("Output:", spiral_memory_steps_1(input_))
+
 
 
 if __name__ == '__main__':
