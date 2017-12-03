04d3aaaf70e0f7b44951269eb0630cad2bd89646
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2017-12-03 02:01:01 +0100

    added less elegant but straight forward solution

--- src/aoc2017/day_2.py
+++ src/aoc2017/day_2.py
@@ -3,11 +3,22 @@
 
 import click
 import numpy as np
+import numpy.ma as ma
 
 from .download_input import get_input
 
 def checksum_puzzle_1(spreadsheet):
-    pass
+    line_split = spreadsheet.split("\n")
+    column_split = [line.split("\t") for line in line_split]
+    max_line_length = max([len(line) for line in column_split])
+    for line in column_split:
+        while len(line) < max_line_length:
+            line.append(-1)
+
+    sheet = ma.masked_equal(ma.array(column_split, dtype=int), -1)
+    checksum = ma.sum(ma.ptp(sheet, axis=1))
+    return checksum
+
 
 
 @click.command()
