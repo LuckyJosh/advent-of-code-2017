ca2d6f3e3391ce01245d0208db8b8f539310c3b5
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2017-12-03 20:45:28 +0100

    added some test cases from the given example

--- tests/test_day_3_puzzle_2.py
+++ tests/test_day_3_puzzle_2.py
@@ -3,3 +3,37 @@
 
 from aoc2017.day_3 import spiral_memory_steps_2
 
+def test_spiral_memory_steps_2_1():
+    input_ = "1"
+    output = 2
+    assert spiral_memory_steps_2(input_) == output
+
+
+def test_spiral_memory_steps_2_2():
+    input_ = "2"
+    output = 4
+    assert spiral_memory_steps_2(input_) == output
+
+
+def test_spiral_memory_steps_2_3():
+    input_ = "3"
+    output = 4
+    assert spiral_memory_steps_2(input_) == output
+
+
+def test_spiral_memory_steps_2_4():
+    input_ = "4"
+    output = 5
+    assert spiral_memory_steps_2(input_) == output
+
+
+def test_spiral_memory_steps_2_5():
+    input_ = "5"
+    output = 10
+    assert spiral_memory_steps_2(input_) == output
+
+
+def test_spiral_memory_steps_2_6():
+    input_ = "11"
+    output = 23
+    assert spiral_memory_steps_2(input_) == output
