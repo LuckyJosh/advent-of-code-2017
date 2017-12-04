2a9ed0bff9cba9bffb3d557189eb4822e440ac1e
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2017-12-04 06:19:48 +0100

    added test cases

--- tests/test_day_4_puzzle_1.py
+++ tests/test_day_4_puzzle_1.py
@@ -1,10 +1,28 @@
 #! /usr/bin/env python
 # encoding: utf-8
 
-from aoc2017.day_ import func_1
+from aoc2017.day_4 import check_passphrase_1
 
 
-def test_func_1_():
-    input_ = ""
-    output =
-    assert func_1(input_) == output
+def test_check_passphrase_1_1():
+    input_ = "aa bb cc dd ee"
+    output = True
+    assert check_passphrase_1(input_) == output
+
+
+def test_check_passphrase_1_2():
+    input_ = "aa bb cc dd aa"
+    output = False
+    assert check_passphrase_1(input_) == output
+
+
+def test_check_passphrase_1_3():
+    input_ = "aa bb cc dd aaa"
+    output = True
+    assert check_passphrase_1(input_) == output
+
+
+def test_check_passphrase_1_4():
+    input_ = ["aa bb cc dd ee", "aa bb cc dd aa", "aa bb cc dd aaa"]
+    output = [True, False, True]
+    assert check_passphrase_1(input_) == output
