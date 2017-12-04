e6fbbb7e1ce3f873525bc67355620488c0e88f3e
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2017-12-04 06:50:36 +0100

    changed test output to reflect the question

--- tests/test_day_4_puzzle_1.py
+++ tests/test_day_4_puzzle_1.py
@@ -6,23 +6,23 @@ from aoc2017.day_4 import check_passphrase_1
 
 def test_check_passphrase_1_1():
     input_ = "aa bb cc dd ee"
-    output = [True]
+    output = 1
     assert check_passphrase_1(input_) == output
 
 
 def test_check_passphrase_1_2():
     input_ = "aa bb cc dd aa"
-    output = [False]
+    output = 0
     assert check_passphrase_1(input_) == output
 
 
 def test_check_passphrase_1_3():
     input_ = "aa bb cc dd aaa"
-    output = [True]
+    output = 1
     assert check_passphrase_1(input_) == output
 
 
 def test_check_passphrase_1_4():
     input_ = "aa bb cc dd ee\naa bb cc dd aa\naa bb cc dd aaa"
-    output = [True, False, True]
+    output = 2
     assert check_passphrase_1(input_) == output
