a260e3e91322b5c918ca3463fb7b29142235c0fa
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2017-12-05 06:19:01 +0100

    added solution for first puzzle

--- src/aoc2017/day_5.py
+++ src/aoc2017/day_5.py
@@ -7,9 +7,20 @@ import numpy as np
 from .download_input import get_input
 
 
-def memory_jumps_1(arg):
-    pass
+def memory_jumps_1(jumps):
+    # @documentation: All or parts of the documentation is missing!
+    jumps = [int(jump) for jump in jumps.split("\n")]
 
+    njumps = len(jumps)
+    i = 0
+    counter = 0
+    while i < njumps:
+        next_i = i + jumps[i]
+        jumps[i] += 1
+        i = next_i
+        counter += 1
+
+    return counter
 
 def func_2(arg):
     pass
