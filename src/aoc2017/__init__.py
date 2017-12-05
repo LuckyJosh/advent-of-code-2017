71bbb490484becbf9a26ed0830dc5d9ea7062d55
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2017-12-05 06:09:16 +0100

    added solution file for day 5

--- src/aoc2017/__init__.py
+++ src/aoc2017/__init__.py
@@ -8,6 +8,7 @@ from . import day_1
 from . import day_2
 from . import day_3
 from . import day_4
+from . import day_5
 
 click_completion.init()
 click_completion.install(shell="fish", prog_name="aoc-2017")
@@ -25,3 +26,4 @@ cli_entry_point.add_command(day_1.main, name="day-1")
 cli_entry_point.add_command(day_2.main, name="day-2")
 cli_entry_point.add_command(day_3.main, name="day-3")
 cli_entry_point.add_command(day_4.main, name="day-4")
+cli_entry_point.add_command(day_5.main, name="day-5")
