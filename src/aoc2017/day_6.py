1963a90147487da016120ce852ef7657e88e907e
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2017-12-06 06:04:25 +0100

    changed function names

--- src/aoc2017/day_6.py
+++ src/aoc2017/day_6.py
@@ -7,19 +7,19 @@ import numpy as np
 from .download_input import get_input
 
 
-def func_1(arg):
+def infinit_loop_1(arg):
     pass
 
 
-def func_2(arg):
+def infinit_loop_2(arg):
     pass
 
 @click.command()
 def main():
     input_ = get_input()
     print("Input:\n", input_)
-    print("Output", func_1(input_))
-    print("Output", func_2(input_))
+    print("Output", infinit_loop_1(input_))
+    print("Output", infinit_loop_2(input_))
 
 
 if __name__ == '__main__':
