5037274a04e166a8ee0ef5af690a10001da4ada3
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2017-12-07 06:23:38 +0100

    fixed test case

--- tests/test_day_7_puzzle_1.py
+++ tests/test_day_7_puzzle_1.py
@@ -5,8 +5,8 @@ from aoc2017.day_7 import towers_1
 
 
 def test_towers_1_1():
-    input_ = "pbga (66)\nxhth (57)\nebii (61)\nhavc (66)\nktlj (57)\nfwft (72) -> ktlj, cntj, xhth\n
-    input_ += "qoyq (66)\npadx (45) -> pbga, havc, qoyq\ntknk (41) -> ugml, padx, fwft\njptl (61)\n
+    input_ = "pbga (66)\nxhth (57)\nebii (61)\nhavc (66)\nktlj (57)\nfwft (72) -> ktlj, cntj, xhth\n"
+    input_ += "qoyq (66)\npadx (45) -> pbga, havc, qoyq\ntknk (41) -> ugml, padx, fwft\njptl (61)\n"
     input_ += "ugml (68) -> gyxo, ebii, jptl\ngyxo (61)\ncntj (57)"
-    output = tknk
+    output = "tknk"
     assert towers_1(input_) == output
