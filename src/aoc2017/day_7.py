267538f84bf5398c5a8e6e366e4ed7a6a0979117
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2017-12-07 18:41:31 +0100

    added horrible solution for 7.2 ... my brain hurts

--- src/aoc2017/day_7.py
+++ src/aoc2017/day_7.py
@@ -8,7 +8,7 @@ from collections import namedtuple
 
 from .download_input import get_input
 
-Node = namedtuple("Node", ["name", "weight", "child_nodes", "parent_node"])
+Node = namedtuple("Node", ["name", "weight", "child_nodes", "parent_node", "weights_child_trees"])
 
 
 def towers_1(program_info):
@@ -23,7 +23,8 @@ def towers_1(program_info):
 
         weight = int(parts[1][1:-1])
         parent_node = []
-        node = Node(parts[0], weight, next_nodes, parent_node)
+        weights_child_trees = []
+        node = Node(parts[0], weight, next_nodes, parent_node, weights_child_trees)
         parsed_programs.append(node)
 
     for child in parsed_programs:
@@ -36,10 +37,85 @@ def towers_1(program_info):
             return prog.name
 
 
+# @hack:  This is horrible but I could not muster the strength to do it nice
+def towers_2(program_info):
+    programs = program_info.split("\n")
+    parsed_programs = []
+    for prog in programs:
+        parts = prog.split(" ")
+        if "->" in parts:
+            next_nodes = [part.rstrip(",") for part in parts[3:]]
+        else:
+            next_nodes = []
+
+        weight = int(parts[1][1:-1])
+        parent_node = []
+        weights_child_trees = []
+        node = Node(parts[0], weight, next_nodes, parent_node, weights_child_trees)
+        parsed_programs.append(node)
+
+    for child in parsed_programs:
+        for parent in parsed_programs:
+            if child.name in parent.child_nodes:
+                child.parent_node.append(parent.name)
+
+    for prog in parsed_programs:
+        if not prog.parent_node:
+            root_prog_name = prog.name
+
+    name_nodes = {}
+    for prog in parsed_programs:
+        name_nodes[prog.name] = prog
+
+    def get_tree_weight(root):
+        tower_weights = {}
+
+        def get_subtree_weight(node):
+            nonlocal tower_weights
+            node = name_nodes[node]
+
+            if not node.child_nodes:
+                tower_weights[node.name] = node.weight
+            else:
+                for child in node.child_nodes:
+                    get_subtree_weight(child)
+                tower_weights[node.name] = node.weight + sum(tower_weights[child] for child in node.child_nodes)
+
+        get_subtree_weight(root)
+        return tower_weights
+
+    sub_tree_weights = get_tree_weight(root_prog_name)
+
+    node_balanced = {}
+    for prog in parsed_programs:
+        child_weights = set(sub_tree_weights[child] for child in prog.child_nodes)
+        node_balanced[prog.name] = len(child_weights) < 2
 
 
-def towers_2(arg):
-    pass
+    def get_heavy_node(root):
+        root = name_nodes[root]
+        if node_balanced[root.name]:
+            return root
+        else:
+            child_sub_tree_weights = [(i, sub_tree_weights[child]) for i, child in enumerate(root.child_nodes)]
+            idx = sorted(child_sub_tree_weights, key=lambda x: x[1], reverse=True)[0][0]
+            node = get_heavy_node(root.child_nodes[idx])
+            return node
+
+
+    heavy_node = get_heavy_node(root_prog_name)
+
+    parent_heavy = name_nodes[heavy_node.parent_node[0]]
+    child_sub_tree_weights = [sub_tree_weights[child] for child in parent_heavy.child_nodes]
+    diff = max(child_sub_tree_weights) - min(child_sub_tree_weights)
+
+    return heavy_node.weight - diff
+
+
+
+
+
+
 
 @click.command()
 def main():
