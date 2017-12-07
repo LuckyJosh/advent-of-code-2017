30e7164f83aef1399b95e467b8fc21af5d1375c9
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2017-12-07 18:40:52 +0100

    added another test case to be sure

--- tests/test_day_7_puzzle_2.py
+++ tests/test_day_7_puzzle_2.py
@@ -10,3 +10,10 @@ def test_towers_2_1():
     input_ += "ugml (68) -> gyxo, ebii, jptl\ngyxo (61)\ncntj (57)"
     output = 60
     assert towers_2(input_) == output
+
+def test_towers_2_2():
+    input_ = "h (3)\nm (1)\nf (2)\ni (3)\nk (1)\nd (9) -> k, l, m\n"
+    input_ += "j (3)\nc (3) -> h, i, j\na (5) -> b, c, d\ng (2)\n"
+    input_ += "b (6) -> e, f, g\ne (3)\nl (1)"
+    output = 2
+    assert towers_2(input_) == output
