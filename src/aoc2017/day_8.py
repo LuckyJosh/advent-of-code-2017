d2a77794c87e4b4f1b4373f7222402f6ebf0efba
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2017-12-08 06:08:42 +0100

    added example as input for testing

--- src/aoc2017/day_8.py
+++ src/aoc2017/day_8.py
@@ -16,7 +16,8 @@ def register_2(arg):
 
 @click.command()
 def main():
-    input_ = get_input(8)
+    #input_ = get_input(8)
+    input_ = "b inc 5 if a > 1\na inc 1 if b < 5\nc dec -10 if a >= 1\nc inc -20 if c == 10"
     print("Input:\n", input_)
     print("Output", register_1(input_))
     print("Output", register_2(input_))
