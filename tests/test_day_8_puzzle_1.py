fb86162e4a371ae16a5e4d9814fec77a99f5a1b1
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2017-12-08 06:06:52 +0100

    added test case from example for 8.1

--- tests/test_day_8_puzzle_1.py
+++ tests/test_day_8_puzzle_1.py
@@ -1,10 +1,10 @@
 #! /usr/bin/env python
 # encoding: utf-8
 
-from aoc2017.day_8 import func_1
+from aoc2017.day_8 import register_1
 
 
-def test_func_1_():
-    input_ = ""
-    output =
-    assert func_1(input_) == output
+def test_register_1_1():
+    input_ = "b inc 5 if a > 1\na inc 1 if b < 5\nc dec -10 if a >= 1\nc inc -20 if c == 10"
+    output = 1
+    assert register_1(input_) == output
