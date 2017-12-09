6175dab9f5827c334b6ad6a81d49876093253b5a
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2017-12-09 08:10:15 +0100

    removed coppied test cases

--- tests/test_day_9_puzzle_1.py
+++ tests/test_day_9_puzzle_1.py
@@ -3,15 +3,6 @@
 
 from aoc2017.day_9 import stream_1
 
-    {}, score of 1.
-    {{{}}}, score of 1 + 2 + 3 = 6.
-    {{},{}}, score of 1 + 2 + 2 = 5.
-    {{{},{},{{}}}}, score of 1 + 2 + 3 + 3 + 3 + 4 = 16.
-    {<a>,<a>,<a>,<a>}, score of 1.
-    {{<ab>},{<ab>},{<ab>},{<ab>}}, score of 1 + 2 + 2 + 2 + 2 = 9.
-    {{<!!>},{<!!>},{<!!>},{<!!>}}, score of 1 + 2 + 2 + 2 + 2 = 9.
-    {{<a!>},{<a!>},{<a!>},{<ab>}}, score of 1 + 2 = 3.
-
 
 def test_stream_1_1():
     input_ = "{}"
