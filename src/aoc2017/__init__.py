5f474e0b0c79f614d399510b5172931d52839fcc
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2017-12-10 09:40:36 +0100

    added cli command for day 10

--- src/aoc2017/__init__.py
+++ src/aoc2017/__init__.py
@@ -13,6 +13,7 @@ from . import day_6
 from . import day_7
 from . import day_8
 from . import day_9
+from . import day_10
 
 click_completion.init()
 click_completion.install(shell="fish", prog_name="aoc-2017")
@@ -35,3 +36,4 @@ cli_entry_point.add_command(day_6.main, name="day-6")
 cli_entry_point.add_command(day_7.main, name="day-7")
 cli_entry_point.add_command(day_8.main, name="day-8")
 cli_entry_point.add_command(day_9.main, name="day-9")
+cli_entry_point.add_command(day_10.main, name="day-10")
