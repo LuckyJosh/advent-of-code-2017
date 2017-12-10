510a5519f3a8eb6b2e93ac1a0fe034c5b2b04f2c
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2017-12-10 20:25:53 +0100

    added solution for 10.2

--- src/aoc2017/day_10.py
+++ src/aoc2017/day_10.py
@@ -40,15 +40,48 @@ def knot_hash_1(lengths, list_length=256):
     return List[0]*List[1]
 
 
-def knot_hash_2(arg):
+def knot_hash_2(lengths, list_length=256):
     # dtype=np.int8
     # array.data.hex() -> two digit hexvalue
-    pass
+    lengths = [ord(char) for char in lengths] + [17, 31, 73, 47, 23]
+
+    lengths = np.array(lengths, dtype=int)
+    repeated_lengths = np.tile(lengths, 64)
+
+    List = np.arange(0, list_length, dtype=np.uint8)
+
+    skip_sizes = np.arange(0, len(repeated_lengths + 1))
+    jumps = (repeated_lengths + skip_sizes) % list_length
+
+    positions = np.zeros_like(skip_sizes)
+    positions[1:] = np.cumsum(jumps[:-1]) % list_length
+
+    ends_of_sublist = repeated_lengths + positions
+
+    wrap_around_by = list_length - ends_of_sublist
+    wrap_around = (wrap_around_by) < 0
+
+    for i in range(len(repeated_lengths)):
+        position = positions[i]
+
+        end_of_sublist = ends_of_sublist[i]
+        if wrap_around[i]:
+            List = np.roll(List, wrap_around_by[i])
+            position = position + wrap_around_by[i]
+            end_of_sublist = list_length
+
+        List[position:end_of_sublist] = List[position:end_of_sublist][::-1]
+
+        if wrap_around[i]:
+            List = np.roll(List, -wrap_around_by[i])
+
+    dense_hash = np.bitwise_xor.reduceat(List, np.arange(0, list_length, 16))
+    dense_hash = dense_hash.astype(np.uint8).data.hex()
+    return dense_hash
 
 @click.command()
 def main():
     input_ = get_input(10)
-    #input_ = "3,4,1,5"
     print("Input:\n", input_)
     print("Output", knot_hash_1(input_))
     print("Output", knot_hash_2(input_))
