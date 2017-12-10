4262ec2aad7286bd45eac493620142d7ef03277d
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2017-12-10 01:41:20 +0100

    added solution for 9.1

--- src/aoc2017/day_9.py
+++ src/aoc2017/day_9.py
@@ -7,8 +7,43 @@ import numpy as np
 from .download_input import get_input
 
 
-def stream_1(arg):
-    pass
+def stream_1(stream):
+    in_garbage = False
+    after_bang = False
+    groups = []
+    current_group = []
+    for i, char in enumerate(stream):
+        if after_bang:
+            after_bang = False
+        elif char == "!":
+            after_bang = True
+        elif char == "<":
+            in_garbage = True
+        elif char == ">":
+            in_garbage = False
+        elif (not in_garbage) and (char == "{"):
+            groups.append([i, 0])
+            current_group.append(len(groups) - 1)
+        elif (not in_garbage) and (char == "}"):
+            groups[current_group.pop()][1] = i
+
+    def get_containment(group):
+        group_containment = []
+        for group_ in groups:
+            if (group_[0] < group[0]) and (group[1] < group_[1]):
+                group_containment.append(True)
+            else:
+                group_containment.append(False)
+        return group_containment
+
+    def get_group_score(group):
+        containment = get_containment(group)
+        return np.cumsum(containment)[-1] + 1
+
+    groups = [tuple(group) for group in groups]
+    score_by_group = {group: get_group_score(group) for group in groups}
+
+    return sum(score_by_group.values())
 
 
 def stream_2(arg):
@@ -16,7 +51,8 @@ def stream_2(arg):
 
 @click.command()
 def main():
-    input_ = get_input()
+    #input_ = "{{{}}}"
+    input_ = get_input(9)
     print("Input:\n", input_)
     print("Output", stream_1(input_))
     print("Output", stream_2(input_))
