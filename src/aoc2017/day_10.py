a209f1c971d548939d003c053eea1af73a47f80d
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2017-12-10 10:46:39 +0100

    added solution for 10.1

--- src/aoc2017/day_10.py
+++ src/aoc2017/day_10.py
@@ -11,9 +11,33 @@ def knot_hash_1(lengths, list_length=256):
     lengths = lengths.split(",")
     lengths = np.array(lengths, dtype=int)
     List = np.arange(0, list_length)
-    skip_sizes = np.arange(0, len(lengths + 1))
 
-    return lengths
+    skip_sizes = np.arange(0, len(lengths + 1))
+    jumps = (lengths + skip_sizes) % list_length
+
+    positions = np.zeros_like(skip_sizes)
+    positions[1:] = np.cumsum(jumps[:-1]) % list_length
+
+    ends_of_sublist = lengths + positions
+
+    wrap_around_by = list_length - ends_of_sublist
+    wrap_around = (wrap_around_by) < 0
+
+    for i in range(len(lengths)):
+        position = positions[i]
+
+        end_of_sublist = ends_of_sublist[i]
+        if wrap_around[i]:
+            List = np.roll(List, wrap_around_by[i])
+            position = position + wrap_around_by[i]
+            end_of_sublist = list_length
+
+        List[position:end_of_sublist] = List[position:end_of_sublist][::-1]
+
+        if wrap_around[i]:
+            List = np.roll(List, -wrap_around_by[i])
+
+    return List[0]*List[1]
 
 
 def knot_hash_2(arg):
@@ -24,7 +48,7 @@ def main():
     #input_ = get_input(10)
     input_ = "3,4,1,5"
     print("Input:\n", input_)
-    print("Output", knot_hash_1(input_))
+    print("Output", knot_hash_1(input_, list_length=5))
     print("Output", knot_hash_2(input_))
 
 
