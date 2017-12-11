b2067580abb5bc5d0fa0503ee6cfefe2eba522ce
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2017-12-11 07:05:11 +0100

    removed custom dtype replaced by simple number index

--- src/aoc2017/day_11.py
+++ src/aoc2017/day_11.py
@@ -11,21 +11,13 @@ def hexgrid_1(movements):
     direction_indices = {"n": 0, "ne": 1, "nw": 2,
                        "s": 3, "se": 4, "sw": 5}
     movements = [direction_indices[move] for move in movements.split(",")]
-    directions_dtype = [("n", int, (4,)),
-                        ("ne", int, (4,)),
-                        ("nw", int, (4,)),
-                        ("s", int, (4,)),
-                        ("se", int, (4,)),
-                        ("sw", int, (4,))]
 
     directions = np.array([(0, 1, 0, 0),
                            (0, 1, 0, 1),
                            (0, 1, 1, 0),
                            (1, 0, 0, 0),
                            (1, 0, 0, 1),
-                           (1, 0, 1, 0)], dtype=directions_dtype)
-
-
+                           (1, 0, 1, 0)])
 
 
     return directions[movements]
