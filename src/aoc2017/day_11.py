b32c634f03ac2a665360f6b0c4a6f49a5a2a0b5d
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2017-12-11 09:07:00 +0100

    fixed solution for 11.1

--- src/aoc2017/day_11.py
+++ src/aoc2017/day_11.py
@@ -7,7 +7,7 @@ import numpy as np
 from .download_input import get_input
 
 
-def hexgrid_1(movements):
+def hexgrid_2(movements):
     direction_indices = {"n": 0, "ne": 1, "nw": 2,
                        "s": 3, "se": 4, "sw": 5}
     movements = [direction_indices[move] for move in movements.split(",")]
@@ -16,12 +16,10 @@ def hexgrid_1(movements):
                            (-1, 0), (-1, 1), (0, -1)])
 
     sum_of_directions = np.sum(directions[movements], axis=0)
-    max_dir = np.abs(sum_of_directions).max()
-    min_dir = np.abs(sum_of_directions).min()
-    if max_dir == min_dir:
-        steps = max_dir
-    else:
-        steps = max_dir + min_dir
+    steps = np.abs(np.sum(sum_of_directions))
+    signs = np.sign(sum_of_directions)
+    if signs[0] != signs[1]:
+        steps += np.abs(sum_of_directions).min()
 
     return steps
 
@@ -29,11 +27,13 @@ def hexgrid_1(movements):
 def hexgrid_2(movements):
     pass
 
+
 @click.command()
 def main():
-    input_ = get_input(11)
+    #input_ = get_input(11)
+    input_ = "ne,ne,ne"
     print("Input:\n", repr(input_))
-    print("Output", hexgrid_1(input_))
+    print("Output", hexgrid_2(input_))
     print("Output", hexgrid_2(input_))
 
 
