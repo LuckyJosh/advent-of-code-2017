f883cc12f2d47a07257703b6ba3537d12dd2e4f2
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2017-12-11 09:08:05 +0100

    fixed function name change mistake

--- src/aoc2017/day_11.py
+++ src/aoc2017/day_11.py
@@ -7,7 +7,7 @@ import numpy as np
 from .download_input import get_input
 
 
-def hexgrid_2(movements):
+def hexgrid_1(movements):
     direction_indices = {"n": 0, "ne": 1, "nw": 2,
                        "s": 3, "se": 4, "sw": 5}
     movements = [direction_indices[move] for move in movements.split(",")]
@@ -49,7 +49,7 @@ def main():
     #input_ = get_input(11)
     input_ = "ne,ne,ne"
     print("Input:\n", repr(input_))
-    print("Output", hexgrid_2(input_))
+    print("Output", hexgrid_1(input_))
     print("Output", hexgrid_2(input_))
 
 
