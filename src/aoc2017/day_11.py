fa6a6ce47cf7f48951c21846f59febfc0634d8f3
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2017-12-11 07:04:29 +0100

    added some code using custom dtypes/structured arrays

--- src/aoc2017/day_11.py
+++ src/aoc2017/day_11.py
@@ -8,7 +8,27 @@ from .download_input import get_input
 
 
 def hexgrid_1(movements):
-    pass
+    direction_indices = {"n": 0, "ne": 1, "nw": 2,
+                       "s": 3, "se": 4, "sw": 5}
+    movements = [direction_indices[move] for move in movements.split(",")]
+    directions_dtype = [("n", int, (4,)),
+                        ("ne", int, (4,)),
+                        ("nw", int, (4,)),
+                        ("s", int, (4,)),
+                        ("se", int, (4,)),
+                        ("sw", int, (4,))]
+
+    directions = np.array([(0, 1, 0, 0),
+                           (0, 1, 0, 1),
+                           (0, 1, 1, 0),
+                           (1, 0, 0, 0),
+                           (1, 0, 0, 1),
+                           (1, 0, 1, 0)], dtype=directions_dtype)
+
+
+
+
+    return directions[movements]
 
 
 def hexgrid_2(movements):
@@ -16,7 +36,8 @@ def hexgrid_2(movements):
 
 @click.command()
 def main():
-    input_ = get_input(11)
+    #input_ = get_input(11)
+    input_ = "ne,ne,ne"
     print("Input:\n", repr(input_))
     print("Output", hexgrid_1(input_))
     print("Output", hexgrid_2(input_))
@@ -24,4 +45,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
