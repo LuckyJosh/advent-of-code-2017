764030f69a731f9a2df537a7436af8a9e8df6d7b
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2017-12-14 14:12:14 +0100

    added rest of solution for 14.1

--- src/aoc2017/day_14.py
+++ src/aoc2017/day_14.py
@@ -12,8 +12,20 @@ def hash_grid_1(key):
     row_keys = [key + "-" + str(i) for i in range(128)]
     row_hashes = [knot_hash_2(row_key) for row_key in row_keys]
 
+    bin_row_hashes = []
+    for row_hash in row_hashes:
+        bin_row_hash = []
+        for char in row_hash:
+            bin_char = bin(int(char, 16))[2:]
+            missing_leading_bits = 4 - len(bin_char)
+            bin_char = "0"*missing_leading_bits + bin_char
+            bin_row_hash.append(bin_char)
+        bin_row_hashes.append("".join(bin_row_hash))
 
-    return row_hashes
+    bin_rows_string = [int(bit) for bit in "".join(bin_row_hashes)]
+
+
+    return sum(bin_rows_string)
 
 def hash_grid_2(arg):
     pass
