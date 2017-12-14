6ca2049e0f844b84ae21cf879740f936a8036bcb
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2017-12-14 12:53:04 +0100

    added parsing of puzzle input for 14.1

--- src/aoc2017/day_14.py
+++ src/aoc2017/day_14.py
@@ -5,18 +5,22 @@ import click
 import numpy as np
 
 from .download_input import get_input
+from day_10 import knot_hash_2
 
 
-def hash_grid_1(arg):
-    pass
+def hash_grid_1(key):
+    row_keys = [key + "-" + str(i) for i in range(128)]
+    row_hashes = [knot_hash_2(row_key) for row_key in row_keys]
 
+    return row_hashes
 
 def hash_grid_2(arg):
     pass
 
 @click.command()
 def main():
-    input_ = get_input(14)
+    #input_ = get_input(14)
+    input_ = "flqrgnkx"
     print("Input:\n", input_)
     print("Output", hash_grid_1(input_))
     print("Output", hash_grid_2(input_))
@@ -24,4 +28,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
