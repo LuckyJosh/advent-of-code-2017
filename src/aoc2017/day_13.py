871f7afb9138de3ac18851bef2c4f5611f987b97
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2017-12-14 04:18:43 +0100

    added (BAD trial-and-error) solution for 13.2

--- src/aoc2017/day_13.py
+++ src/aoc2017/day_13.py
@@ -3,6 +3,7 @@
 
 import click
 import numpy as np
+import tqdm
 
 from .download_input import get_input
 
@@ -63,9 +64,67 @@ def firewall_1(depth_range):
     return np.sum(caught_indices * depth_range[caught_indices])
 
 
+def firewall_2(depth_range):
 
-def firewall_2(arg):
-    pass
+    depth_range = {int(line.split()[0]): int(line.split()[-1])
+                   for line in depth_range.replace(":", "").split("\n")}
+
+    depth_range_ = []
+
+    for i in range(max(depth_range) + 1):
+        if i in depth_range:
+            depth_range_.append(depth_range[i])
+        else:
+            depth_range_.append(-1)
+
+    depth_range = np.array(depth_range_)
+
+    max_depth = len(depth_range)
+    max_range = max(depth_range)
+
+    def pyramid_range(max_num, num_repeats=0, cutoff=None):
+        num_elements = max_num + max_num - 1
+        if num_repeats == 0 and cutoff is not None:
+            num_repeats = cutoff//(num_elements - 1)
+
+        pyramid_range = np.full(num_elements, max_num - 1)
+        indicies = np.arange(max_num - 1)
+        pyramid_range[indicies] = indicies
+        pyramid_range[indicies+max_num] = indicies[::-1]
+        num_total_elements = num_elements + (num_elements - 1) * num_repeats
+        repeated_pyramid_range = np.zeros(shape=num_total_elements, dtype=int)
+
+        if num_repeats > 0:
+            repeated_pyramid_range[:-num_elements] = np.tile(pyramid_range[:-1], num_repeats)
+
+        repeated_pyramid_range[-num_elements:] = pyramid_range
+        if cutoff is not None:
+            repeated_pyramid_range = repeated_pyramid_range[:cutoff]
+
+        return repeated_pyramid_range
+
+
+
+
+    depth_index = np.arange(max_depth)
+    layers = []
+    for range_ in depth_range:
+        if range_ != -1:
+            layers.append(pyramid_range(range_, cutoff=50000*max_depth))
+        else:
+            layers.append(np.full(50000*max_depth, range_))
+
+    positions_in_time = np.column_stack(layers)
+    delay = None
+    for i in tqdm.tqdm(range(0, 49999*max_depth)):
+        caught_indices = np.argwhere(np.diag(positions_in_time[i:i + max_depth,:]) == 0).flatten()
+        #print(positions_in_time[i:i + max_depth, :])
+        #print(np.diag(positions_in_time[i:i + max_depth, :]))
+        if len(caught_indices) == 0:
+            delay = i
+            break
+    return delay
+
 
 @click.command()
 def main():
