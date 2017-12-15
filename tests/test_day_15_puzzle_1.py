2e2e9d49c848f6f7f6c9e29b9ca8f7aed8ad8548
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2017-12-15 15:15:47 +0100

    added test file for 15.1

---
+++ tests/test_day_15_puzzle_1.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+# encoding: utf-8
+
+from aoc2017.day_15 import generators_1
+
+
+def test_generators_1_1():
+    input_ = ""
+    output =
+    assert  generators_1_1(input_) == output
