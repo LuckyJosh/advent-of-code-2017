24511d65a51ef8bb44186e992b9050140dc8830b
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2017-12-15 16:06:57 +0100

    fixed test input

--- tests/test_day_15_puzzle_1.py
+++ tests/test_day_15_puzzle_1.py
@@ -5,6 +5,6 @@ from aoc2017.day_15 import generators_1
 
 
 def test_generators_1_1():
-    input_ = "Generator A starts with 703\nGenerator B starts with 516"
+    input_ = "Generator A starts with 65\nGenerator B starts with 8921"
     output = 588
-    assert  generators_1_1(input_) == output
+    assert  generators_1(input_) == output
