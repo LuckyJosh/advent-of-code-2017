33c2ea4b520e7b6da2c0307f0c8f944fe78613c3
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2017-12-15 15:17:24 +0100

    added testcase for 15.1

--- tests/test_day_15_puzzle_1.py
+++ tests/test_day_15_puzzle_1.py
@@ -5,6 +5,6 @@ from aoc2017.day_15 import generators_1
 
 
 def test_generators_1_1():
-    input_ = ""
-    output =
+    input_ = "Generator A starts with 703\nGenerator B starts with 516"
+    output = 588
     assert  generators_1_1(input_) == output
