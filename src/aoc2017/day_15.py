90f01f479148c668948ebf71b4fe6fe3e4c68844
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2017-12-15 15:20:56 +0100

    added input parsing and example input to 15.1

--- src/aoc2017/day_15.py
+++ src/aoc2017/day_15.py
@@ -7,16 +7,17 @@ import numpy as np
 from .download_input import get_input
 
 
-def generators_1(arg):
-    pass
-
+def generators_1(seedstring):
+    seeds = [int(line.split()[-1]) for line in seedstring.split("\n")]
+    return seeds
 
 def generators_2(arg):
     pass
 
 @click.command()
 def main():
-    input_ = get_input()
+    #input_ = get_input()
+    input_ = "Generator A starts with 703\nGenerator B starts with 516"
     print("Input:\n", input_)
     print("Output", generators_1(input_))
     print("Output", generators_2(input_))
