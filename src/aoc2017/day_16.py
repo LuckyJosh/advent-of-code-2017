6ffa2088ee7eef421fc76c77005156cea4d9f91a
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2017-12-16 18:12:58 +0100

    changed input to test input for the  solution building

--- src/aoc2017/day_16.py
+++ src/aoc2017/day_16.py
@@ -16,7 +16,8 @@ def permutations_2(instructions):
 
 @click.command()
 def main():
-    input_ = get_input(16)
+    #input_ = get_input(16)
+    input_ = "s1,x3/4,pe/b"
     print("Input:\n", input_)
     print("Output", permutations_1(input_))
     print("Output", permutations_2(input_))
@@ -24,4 +25,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
