b3bf8634434c4741e557841919da01ed9c4e0446
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2017-12-17 17:45:53 +0100

    started on idea for faster solution

--- src/aoc2017/day_17.py
+++ src/aoc2017/day_17.py
@@ -21,25 +21,24 @@ def spinlock_1(num_steps):
         spinlock.insert(current_position + 1, it)
         len_spinlock += 1
         current_position += 1
-    print(spinlock)
+
     return spinlock[(spinlock.index(2017) + 1) % len_spinlock]
 
 
 def spinlock_2(num_steps):
     num_steps = int(num_steps)
-    spinlock = [0]
-    len_spinlock = 1
     num_iterations = int(5e6)
-    current_position = 0
+    spinlock = [0]
+    len_spinlock = np.arange(1, num_iterations + 1, dtype=int)
+    current_position = np.zeros_like(len_spinlock)
+    for i in range(1, num_iterations):
+        current_position[i] = (current_position[i-1] + num_steps) % len_spinlock[i-1] + 1
 
-    for it in tqdm(range(1, num_iterations + 1)):
-        current_position += num_steps
-        current_position %= len_spinlock
-        spinlock.insert(current_position + 1, it)
-        len_spinlock += 1
-        current_position += 1
-    print(spinlock)
-    return spinlock[(spinlock.index(0) + 1) % len_spinlock]
+    pos_of_zero = np.sum(current_position[1:] == 0)
+    inserted_after_zero_pos = np.argwhere(current_position == (pos_of_zero + 1))
+    last_inserted = np.max(inserted_after_zero_pos)
+
+    return last_inserted
 
 @click.command()
 def main():
