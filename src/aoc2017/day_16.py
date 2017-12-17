1594ec6488fbadefe54866a596d288200aecc5e6
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2017-12-17 01:19:02 +0100

    got a solution evantually ... lost my mind in the process ...

--- src/aoc2017/day_16.py
+++ src/aoc2017/day_16.py
@@ -52,62 +52,62 @@ def permutations_2(instructions, num_programs=16):
     parsed_instructions = []
     for inst in instructions:
         inst_type = inst[0]
-        inst_args = tuple([int(i) if i.isdigit() else (ord(i) - ascii_offset)
-                           for i in inst[1:].split("/")])
+        inst_args = tuple([int(arg) if arg.isdigit() else ord(arg) - ascii_offset
+                           for arg in inst[1:].split("/")])
         parsed_instructions.append((inst_type, inst_args))
 
+
     def exchange(positions, ind1, ind2):
-        positions[[ind1, ind2]] = positions[[ind2, ind1]]
+        positions[ind1], positions[ind2] = positions[ind2], positions[ind1]
         return positions
 
-    def partner(positions, ind1, ind2):
-        ind1, ind2 = np.argwhere(positions == ind1)[0,0], np.argwhere(positions == ind2)[0,0]
-        positions[[ind1, ind2]] = positions[[ind2, ind1]]
+    def partner(positions, partner1, partner2):
+        ind1 = positions.index(partner1)
+        ind2 = positions.index(partner2)
+        positions = exchange(positions, ind1, ind2)
         return positions
 
     def spin(positions, steps):
-        positions = np.roll(positions, steps)
+        positions = positions[-steps:] + positions[:-steps]
         return positions
 
-    program_positions = np.arange(num_programs)
+    program_positions = list(range(num_programs))
 
     possible_moves = {"s": spin,
                       "p": partner,
                       "x": exchange}
 
-    for inst in parsed_instructions:
 
-        program_positions = possible_moves[inst[0]](program_positions, *inst[1])
+    def alldances(program_positions, num_dances):
+        start_positions = program_positions[:]
+        for i in tqdm(range(num_dances)):
+            for inst in parsed_instructions:
+                program_positions = possible_moves[inst[0]](program_positions, *inst[1])
+
+            if program_positions == start_positions:
+                break
+
+        necessary_dances = num_dances % (i + 1)
+
+        for i in tqdm(range(necessary_dances)):
+            for inst in parsed_instructions:
+                start_positions = possible_moves[inst[0]](start_positions, *inst[1])
+        return start_positions
 
 
-    dances = int(1e9)
-    binary_dances = bin(dances)[2:]
-    num_bits_dances = len(binary_dances)
-    dances_mask = np.array([int(bit) for bit in binary_dances], dtype=bool)
+    dances = 1_000_000_000
+
+    program_positions = alldances(program_positions, dances)
+
+    return "".join([chr(prog + ascii_offset) for prog in program_positions])
 
 
-    permutations = np.zeros((num_bits_dances, num_programs), dtype=int)
-    permutations[0, :] = program_positions
 
-    for i in range(1, num_bits_dances):
-        permutations[i, :] = permutations[i - 1, :][permutations[i - 1, :]]
-
-    selected_permutations = permutations[dances_mask, :]
-
-    print(permutations)
-    print(dances_mask)
-    print(selected_permutations)
-
-    program_positions = np.arange(num_programs)
-    for permutation in selected_permutations:
-        program_positions = program_positions[permutation]
-
-    return "".join([chr(prog) for prog in program_positions + ascii_offset])
 
 @click.command()
 def main():
     input_ = get_input(16)
-    #input_ = "s1,x3/4,pc/b"
+    #input_ = "s1,x3/4,pe/b"
     print("Input:\n", input_)
     print("Output", permutations_1(input_))
     print("Output", permutations_2(input_))
