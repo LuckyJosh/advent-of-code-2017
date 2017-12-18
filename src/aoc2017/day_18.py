977606e6c60c038a0b6f4cef32bb87ec6b9e1c44
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2017-12-18 16:42:34 +0100

    added parsing of instructions

--- src/aoc2017/day_18.py
+++ src/aoc2017/day_18.py
@@ -4,20 +4,31 @@
 import click
 import numpy as np
 
+
 from .download_input import get_input
 
 
-def duet_1(instructions):
 
+def duet_1(instructions):
+    instructions = instructions.split("\n")
+    parsed_instructions = []
+    for inst in instructions:
+        inst_parts = [part if part.isalpha() else int(part)
+                      for part in inst.split(" ")]
+
+        parsed_instructions.append((inst_parts[0], tuple(inst_parts[1:])))
+
+    return parsed_instructions
 
 
 def duet_2(instructions):
     pass
 
+
 @click.command()
 def main():
     input_ = "set a 1\nadd a 2\nmul a a\nmod a 5\nsnd a\nset a 0\nrcv a\njgz a -1\nset a 1\njgz a -2"
-    input_ = get_input(18)
+#    input_ = get_input(18)
     print("Input:\n", input_)
     print("Output", duet_1(input_))
     print("Output", duet_2(input_))
