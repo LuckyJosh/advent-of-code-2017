d48228af48798e489b7b19fd43ec861315a057dd
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2017-12-18 17:24:10 +0100

    added solution for 18.1

--- src/aoc2017/day_18.py
+++ src/aoc2017/day_18.py
@@ -3,7 +3,7 @@
 
 import click
 import numpy as np
-
+from collections import defaultdict
 
 from .download_input import get_input
 
@@ -18,7 +18,88 @@ def duet_1(instructions):
 
         parsed_instructions.append((inst_parts[0], tuple(inst_parts[1:])))
 
-    return parsed_instructions
+    last_played_frequency = 0
+    registers = defaultdict(int)
+
+    def snd(x):
+        nonlocal last_played_frequency
+        nonlocal registers
+        nonlocal i
+        if isinstance(x, str):
+            x = registers[x]
+        last_played_frequency = x
+        i += 1
+
+    def set(x, y):
+        nonlocal registers
+        nonlocal i
+        if isinstance(y, str):
+            y = registers[y]
+        registers[x] = y
+        i += 1
+
+    def add(x, y):
+        nonlocal registers
+        nonlocal i
+        if isinstance(y, str):
+            y = registers[y]
+        registers[x] += y
+        i += 1
+
+    def mul(x, y):
+        nonlocal registers
+        nonlocal i
+        if isinstance(y, str):
+            y = registers[y]
+        registers[x] *= y
+        i += 1
+
+    def mod(x, y):
+        nonlocal registers
+        nonlocal i
+        if isinstance(y, str):
+            y = registers[y]
+        registers[x] %= y
+        i += 1
+
+    def rcv(x):
+        nonlocal last_played_frequency
+        nonlocal registers
+        nonlocal i
+        if isinstance(x, str):
+            x = registers[x]
+        i += 1
+        if x != 0:
+            freq = last_played_frequency
+            return freq
+
+    def jgz(x, y):
+        nonlocal registers
+        nonlocal i
+        if isinstance(x, str):
+            x = registers[x]
+        if x > 0:
+            i += y
+        else:
+            i += 1
+
+    operations = {"snd": snd,
+                  "set": set,
+                  "add": add,
+                  "mul": mul,
+                  "mod": mod,
+                  "rcv": rcv,
+                  "jgz": jgz}
+
+    i = 0
+    while 0 <= i < len(parsed_instructions):
+        current_inst = parsed_instructions[i]
+        print("Current Instruction:", current_inst)
+        print(i)
+        freq = operations[current_inst[0]](*current_inst[1])
+        if freq is not None:
+            return freq
+
 
 
 def duet_2(instructions):
