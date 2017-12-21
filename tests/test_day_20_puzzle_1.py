cf236e5d710b9941457b989d2129a3d8a50f0ad6
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2017-12-21 01:35:55 +0100

    added test case for 20.1

---
+++ tests/test_day_20_puzzle_1.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+# encoding: utf-8
+
+from aoc2017.day_20 import particles_1
+
+
+def test_particles_1_1():
+    input_ = "p=< 3,0,0>, v=< 2,0,0>, a=<-1,0,0>\np=< 4,0,0>, v=< 0,0,0>, a=<-2,0,0> "
+    output = 0
+    assert particles_1(input_) == output
+
+
+
+
