af560e9e96f645d1e9a57b2c9e31e99c19a61964
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2017-12-21 01:12:38 +0100

    fixed function bound to subcomand

--- src/aoc2017/__init__.py
+++ src/aoc2017/__init__.py
@@ -56,4 +56,4 @@ cli_entry_point.add_command(day_16.main, name="day-16")
 cli_entry_point.add_command(day_17.main, name="day-17")
 cli_entry_point.add_command(day_18.main, name="day-18")
 
-cli_entry_point.add_command(day_18.main, name="day-20")
+cli_entry_point.add_command(day_20.main, name="day-20")
