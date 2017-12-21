48b68f52ef87a575d3ca578076e7cdd1ff18361d
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2017-12-21 01:50:25 +0100

    fixed whitespace in test input

--- tests/test_day_20_puzzle_1.py
+++ tests/test_day_20_puzzle_1.py
@@ -5,7 +5,7 @@ from aoc2017.day_20 import particles_1
 
 
 def test_particles_1_1():
-    input_ = "p=< 3,0,0>, v=< 2,0,0>, a=<-1,0,0>\np=< 4,0,0>, v=< 0,0,0>, a=<-2,0,0> "
+    input_ = "p=<3,0,0>, v=<2,0,0>, a=<-1,0,0>\np=<4,0,0>, v=<0,0,0>, a=<-2,0,0> "
     output = 0
     assert particles_1(input_) == output
 
