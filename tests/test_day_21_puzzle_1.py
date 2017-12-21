e9d81b74faabbf9a92defad97886b694746d8d0c
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2017-12-21 22:17:51 +0100

    added additional function argument

--- tests/test_day_21_puzzle_1.py
+++ tests/test_day_21_puzzle_1.py
@@ -7,4 +7,4 @@ from aoc2017.day_21 import fractral_1
 def test_fractral_1_():
     input_ = "../.# => ##./#../...\n.#./..#/### => #..#/..../..../#..#"
     output = 12
-    assert fractral_1(input_) == output
+    assert fractral_1(input_, num_iterations=2) == output
