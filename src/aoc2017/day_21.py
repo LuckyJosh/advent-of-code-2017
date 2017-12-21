a046dfc23a12b63f90f25f3b9e167314179f0b82
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2017-12-21 13:26:06 +0100

    added permutations for 2x2 and 3x3 arrays

--- src/aoc2017/day_21.py
+++ src/aoc2017/day_21.py
@@ -8,7 +8,22 @@ from .download_input import get_input
 
 
 def fractral_1(rules):
-    pass
+    # flip on y-axis, flip on x-axis, rotate 90°, rotate 180°, rotate 270°
+    # elements that stay the same are skipped
+    # (x, y, z) :
+    # permuted.flatten[y] = array.flatten()[x]
+    # permuted.flatten[z] = array.flatten()[y]
+    # permuted.flatten[x] = array.flatten()[z]
+
+    premutations_2by2 = [((0, 1), (2, 3)), ((0, 2), (1, 3)),
+                         ((0, 1, 2, 3),), ((0, 2), (1, 3)), ((0, 2, 3, 1))]
+
+    permutation_3by3 = [((0, 2), (3, 5), (6, 8)), ((0, 6), (1, 7), (2, 8)),
+                        ((0, 2, 8, 6), (1, 5, 7, 3)), ((0, 8), (1, 7), (2, 6), (3, 5)), ((0, 6, 8, 2), (1, 3, 7, 5))]
+
+    rules = rules.split("\n")
+
+
 
 
 def fractral_2(rules):
