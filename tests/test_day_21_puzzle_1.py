78a61670fd45d8bf6e3086043f0300c201fe2898
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2017-12-21 13:07:45 +0100

    added test case for 21.1

---
+++ tests/test_day_21_puzzle_1.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+# encoding: utf-8
+
+from aoc2017.day_21 import fractral_1
+
+
+def test_fractral_1_():
+    input_ = "../.# => ##./#../...\n.#./..#/### => #..#/..../..../#..#"
+    output = 12
+    assert fractral_1(input_) == output
