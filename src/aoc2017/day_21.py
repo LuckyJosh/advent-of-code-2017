35945094a8fd300077e6cb61cc46fe5187195162
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2017-12-21 15:09:42 +0100

    added permutation of rule input

--- src/aoc2017/day_21.py
+++ src/aoc2017/day_21.py
@@ -16,19 +16,46 @@ def fractral_1(rules):
     # permuted.flatten[x] = array.flatten()[z]
 
     premutations_2by2 = [((0, 1), (2, 3)), ((0, 2), (1, 3)),
-                         ((0, 1, 2, 3),), ((0, 2), (1, 3)), ((0, 2, 3, 1))]
+                             ((0, 1, 2, 3),), ((0, 2), (1, 3)), ((0, 2, 3, 1),)]
 
     permutation_3by3 = [((0, 2), (3, 5), (6, 8)), ((0, 6), (1, 7), (2, 8)),
-                        ((0, 2, 8, 6), (1, 5, 7, 3)), ((0, 8), (1, 7), (2, 6), (3, 5)), ((0, 6, 8, 2), (1, 3, 7, 5))]
+                            ((0, 2, 8, 6), (1, 5, 7, 3)), ((0, 8), (1, 7), (2, 6), (3, 5)), ((0, 6, 8, 2), (1, 3, 7, 5))]
 
     flattend_index_2by2 = [(0, 0), (0, 1),
-                           (1, 0), (1, 1)]
+                               (1, 0), (1, 1)]
 
     flattend_index_3by3 = [(0, 0), (0, 1), (0, 2),
                            (1, 0), (1, 1), (1, 2),
                            (2, 0), (2, 1), (2, 2)]
+    def premute_string(string, permutation, dim):
+        string_list = [char for char in string]
+        new_string_list = string_list.copy()
+
+        for step in permutation:
+            step = list(step)
+            step += [step[0]]
+            for i in range(0, len(step)-1):
+                old_idx, new_idx = step[i], step[i+1]
+                new_string_list[new_idx] = string_list[old_idx]
+
+
+        string_list = new_string_list
+        return "".join(string_list)
+
     rules = rules.replace("/","").replace(" ", "").split("\n")
     rules = {rule.split("=>")[0]: rule.split("=>")[1] for rule in rules}
+
+    rules_extension = {}
+    for rule_input, rule_output in rules.items():
+        new_input = ""
+        if len(rule_input) == 4:
+            for permutation in premutations_2by2:
+                new_input = premute_string(rule_input, permutation, dim=2)
+                rules_extension[new_input] = rule_output
+
+    rules.update(rules_extension)
+
+
     return rules
 
 
