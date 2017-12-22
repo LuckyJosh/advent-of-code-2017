d7fa872ca80f9632867cd9be0bf12bde293891c9
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2017-12-22 18:36:18 +0100

    added argument unpacking to test cases

--- tests/test_day_22_puzzle_1.py
+++ tests/test_day_22_puzzle_1.py
@@ -7,16 +7,16 @@ from aoc2017.day_22 import virus_1
 def test_virus_1_1():
     input_ = ("..#\n#..\n...", 7)
     output = 5
-    assert virus_1(input_) == output
+    assert virus_1(*input_) == output
 
 
 def test_virus_1_2():
     input_ = ("..#\n#..\n...", 70)
     output = 41
-    assert virus_1(input_) == output
+    assert virus_1(*input_) == output
 
 
 def test_virus_1_3():
     input_ = ("..#\n#..\n...", 10000)
     output = 5587
-    assert virus_1(input_) == output
+    assert virus_1(*input_) == output
